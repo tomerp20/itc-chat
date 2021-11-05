e18652c01fa01ebed21abdf5e075aafb635e2179
= <tomerp20@users.noreply.github.com>
2021-11-05 12:09:04 +0200

    finish auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import { useState, useEffect, useContext } from 'react'
-import styles from './styles/app.module.css'
-import MessagesList from './components/MessagesList'
-import AddNewMessage from './components/AddNewMessage'
-import { getFirestore, addDoc, collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
-import { FirebaseContext } from './utils/Firebase'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Chat from './components/Chat';
 import Login from './components/Login'
 import { AuthContext } from './components/AuthContext';
+import { Fragment } from 'react';
 function App() {
   const [authInfo, setAuthInfo] = useState(null)
+  const [isauthfinished, setIsauthfinished] = useState(false)
   const logout = () => {
     setAuthInfo(null)
-    //
+    
   }
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) 
+      setAuthInfo({name:user.displayName, email:user.email})
+       else {
+      }
+      setIsauthfinished(true)
+});
+  },[])
   return (
+    <Fragment>
+      {isauthfinished ?
+      <div>
+        <AuthContext.Provider value={{
+          authInfo,
+          login: (userInfo) => setAuthInfo(userInfo),
+          logout,
+        }}>
+          {!authInfo && <Login />}
+          {authInfo && <Chat />}
+        </AuthContext.Provider>
+      </div>
+      : 
     <div>
-      <AuthContext.Provider value={{
-        authInfo,
-        login: (userInfo) => setAuthInfo(userInfo),
-        logout,
-      }}>
-        {!authInfo && <Login />}
-        {authInfo && <Chat />}
-      </AuthContext.Provider>
+      Loading...
     </div>
+    }
+  </Fragment>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,18 @@
-import React from 'react'
+import { useContext } from 'react'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { AuthContext } from './AuthContext';
 
 export default function Login() {
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
-
-    const handleClick = () => {
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log(result)
-            console.log(user)
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
+    const { login } = useContext(AuthContext)
+    const handleClick = async () => {
+        try {
+            const userData = await signInWithPopup(auth, provider)
+            login({name:userData.user.displayName, email:userData.user.email})
+        } catch (error) {
+         console.log(error)
+        }
     }
     
     return (
